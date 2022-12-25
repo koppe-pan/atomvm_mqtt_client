4f0a7b46d11b27f95a7be373156801efa78d2848
koppe_pan <takas3141@gmail.com>
2022-12-25 19:00:31 +0900

    Try connecting to server
    Compatible with `initial commit` of server

--- src/mqtt_client_example.erl
+++ src/mqtt_client_example.erl
@@ -37,38 +37,6 @@ start() ->
 
     loop_forever().
 
-loop_forever() ->
-    receive
-        halt -> halt
-    end.
-
-%%
-%% connected callback.  This function will be called
-%%
-handle_connected(MQTT) ->
-    Config = mqtt_client:get_config(MQTT),
-    Topic = <<"atomvm/qos0">>,
-    io:format("Connected to ~p~n", [maps:get(url, Config)]),
-    io:format("Subscribing to ~p...~n", [Topic]),
-    ok = mqtt_client:subscribe(MQTT, Topic, #{
-        subscribed_handler => fun handle_subscribed/2,
-        data_handler => fun handle_data/3
-    }).
-
-handle_subscribed(MQTT, Topic) ->
-    io:format("Subscribed to ~p.~n", [Topic]),
-    io:format("Spawning publish loop on topic ~p~n", [Topic]),
-    spawn(fun() -> publish_loop(MQTT, Topic, 1) end).
-
-handle_data(_MQTT, Topic, Data) ->
-    io:format("Received data on topic ~p: ~p ~n", [Topic, Data]),
-    % io:format("Pending publishes: ~p~n", [mqtt_client:get_pending_publishes(MQTT)]),
-    % io:format("Pending subscriptions: ~p~n", [mqtt_client:get_pending_subscriptions(MQTT)]),
-    % io:format("Pending unsubscriptions: ~p~n", [mqtt_client:get_pending_unsubscriptions(MQTT)]),
-    io:format("process count: ~p~n", [erlang:system_info(process_count)]),
-    io:format("Free heap on handle_data: ~p~n", [erlang:system_info(esp32_free_heap_size)]),
-    ok.
-
 start_network(StaConfig) ->
     case network_fsm:wait_for_sta(StaConfig) of
         {ok, {Address, Netmask, Gateway}} ->
@@ -81,10 +49,30 @@ start_network(StaConfig) ->
             throw({unable_to_start_network, Error})
     end.
 
-publish_loop(MQTT, Topic, Seq) ->
-    io:format("Publishing data on topic ~p~n", [Topic]),
-    _ = mqtt_client:publish(MQTT, Topic, list_to_binary("echo" ++ integer_to_list(Seq))),
-    timer:sleep(5000),
-    io:format("process count: ~p~n", [erlang:system_info(process_count)]),
-    io:format("Free heap after publish: ~p~n", [erlang:system_info(esp32_free_heap_size)]),
-    publish_loop(MQTT, Topic, Seq + 1).
+loop_forever() ->
+    receive
+        halt -> halt
+    end.
+
+%%
+%% connected callback.  This function will be called
+%%
+handle_connected(MQTT) ->
+    Config = mqtt_client:get_config(MQTT),
+    SubscribeTopic = list_to_binary("commands/" ++ maps:get(clientid, config:get()) ++ "/set_interval"),
+    io:format("Connected to ~p~n", [maps:get(url, Config)]),
+    io:format("Subscribing to ~p...~n", [SubscribeTopic]),
+    ok = mqtt_client:subscribe(MQTT, SubscribeTopic, #{
+        subscribed_handler => fun handle_subscribed/2,
+        data_handler => fun handle_data/3
+    }).
+
+handle_subscribed(MQTT, SubscribeTopic) ->
+    io:format("Subscribed to ~p.~n", [SubscribeTopic]),
+    PublishTopic = list_to_binary("register/" ++ maps:get(clientid, config:get())),
+    _ = mqtt_client:publish(MQTT, PublishTopic, maps:get(clientid, config:get())).
+
+handle_data(MQTT, SubscribeTopic, Data) ->
+    io:format("Received data on topic ~p: ~p ~n", [SubscribeTopic, Data]),
+    PublishTopic = list_to_binary("reports/" ++ maps:get(clientid, config:get())),
+    _ = mqtt_client:publish(MQTT, PublishTopic, Data).
